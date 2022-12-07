c562c7ec8f13356736a38f83d8e94581b58533d0
unknown <magp2606@gmail.com>
2022-12-07 18:51:00 -0500

    Make voting persist in db

--- src/server/utils/prisma.ts
+++ src/server/utils/prisma.ts
@@ -7,7 +7,4 @@ declare global {
 export const prisma = global.prisma || new PrismaClient({ log: ['query'] })
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
-// database: t3-first-app-roundest-pokemon
-// username: e9f7iwmkkzug7z1xmght
-// host: us-west.connect.psdb.cloud
-// password: pscale_pw_5V9cGveQ6sSOogAKjqJ2fu90Y1zDTC4UdcdWSOYG2iL
+

bf4b278f27b2489e9f32b98e72d32a893647fb47
unknown <magp2606@gmail.com>
2022-12-07 12:50:26 -0500

    Folder structure edited and added trpc basic setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { trpc } from '../utils/trpc'
 
 export default function Home() {
+  const { data, isLoading } = trpc.hello.useQuery({ text: 'Marco' })
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (data) {
+    return <p>{data.greeting}</p>
+  }
+
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
       <div className=' text-2xl text-center'>Which Pokemon is rounder?</div>
-      <div className='p-2'/>
+      <div className='p-2' />
       <div className=' border rounded p-8 flex justify-between max-w-2xl items-center'>
         <div className='w-16 h-16 bg-red-400' />
         <div className='p-8'>VS</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: 'jit',
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }

888c0439d615ba7e1585585ec204a28e07ca93af
unknown <magp2606@gmail.com>
2022-12-07 19:34:18 -0500

    Added Head and Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Head from 'next/head'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
@@ -29,6 +30,9 @@ export default function Home() {
   }
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center '>
+      <Head>
+        <title>Who's the roundest pokemon?</title>
+      </Head>
       <div className=' text-2xl text-center'>Which Pokemon is rounder?</div>
       <div className='p-2' />
       <div className=' border rounded p-10 flex justify-between max-w-2xl items-center '>
